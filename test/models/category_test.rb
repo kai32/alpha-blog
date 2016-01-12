ef6c9ddf954b56ac4b9b74b684092f666a9f3da7
Kai Xiang <kaixiang@refvy.com>
2016-01-12 06:14:59 +0000

    Add categories model and tests

---
+++ test/models/category_test.rb
@@ -0,0 +1,39 @@
+require 'test_helper'
+
+class CategoryTest < ActiveSupport::TestCase
+  
+  def setup
+    @category = Category.new(name: "sports")
+  end
+  
+  test "category should be valid" do
+    assert @category.valid?
+  end
+  
+  test "name should be present" do
+    @category.name = " "
+    assert_not @category.valid?
+  end
+  
+  test "name should be unique" do
+    @category.save
+    category2 = Category.new(name: "sports")
+    assert_not category2.valid?
+  end
+  
+  test "name should not be too long" do
+    @category.name = "a" * 26
+    assert_not @category.valid?
+  end
+  
+  test "name should not be too short" do
+    @category.name = "a" * 2
+    assert_not @category.valid?
+  end
+  
+  test "name can be 25 characters long" do
+    @category.name = "a" * 25
+    assert @category.valid?
+  end
+  
+end
